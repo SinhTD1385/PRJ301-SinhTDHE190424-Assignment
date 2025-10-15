509d1d216e6938b4ca68331a5b664681d3a03a2d
SinhTD1385 <sinhtd1308@gmail.com>
2025-10-15 09:04:16 +0700

    connect SQL server to project

---
+++ Ass_HE190424_System_Manage_LeaveApplication/src/java/dal/DBContext.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Admin
+ */
+public class DBContext {
+    private final String url = "jdbc:sqlserver://MAGIIIIt:1433;"
+            + "databaseName=Ass_HE190424_System_Manage_LeaveApplication;"
+            + "encrypt=true;trustServerCertificate=true;";
+    
+    private final String user = "magi";
+    private final String pass = "123";
+    
+    public Connection getConnection() throws Exception {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
